8b667efa1f109321b894a0fc8277d9261fe4e827
Matthew Basson <matthew.basson@lexisnexis.co.uk>
2022-09-10 13:49:12 +0100

    Adding configuration changes.

--- src/ExpensifyImporter/ExpensifyImporter.Application/Program.cs
+++ src/ExpensifyImporter/ExpensifyImporter.Application/Program.cs
@@ -1,6 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ExpensifyImporter.Application;
 using ExpensifyImporter.Application.Data;
+using ExpensifyImporter.Library.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MySqlConnector;
+using System.Reflection;
 
 var flagExtractor = new FlagExtractor(args);
 
@@ -21,8 +28,34 @@ if(flags.Any(a=>a.Flag == Flags.Help))
 if(flags.Any(a=>a.Flag == Flags.Directory))
 {
     //Directory flag specified processing can begin and resource newing up can start.
+    IConfigurationRoot configuration;
+    var builder = Host.CreateDefaultBuilder(args)
+        .ConfigureAppConfiguration((hostingContext, config) =>
+        {
+            config.SetBasePath(Directory.GetCurrentDirectory());
+            config.AddJsonFile("appsettings.json", true, true);
+            config.AddEnvironmentVariables();
+            config.AddUserSecrets(Assembly.GetExecutingAssembly(), true);
+            configuration = config.Build();
+        })        
+        .ConfigureServices((services) =>
+        {
+            services.AddDbContext<ExpensifyContext>((provider, options) =>
+            {
+                var connectionString = new MySqlConnectionStringBuilder()
+                {
 
-    
+                }.ToString();
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), builder =>
+                {
+                    builder.CommandTimeout(60);
+                });
+            });
+        });
+
+    var app = builder.Build();
+
+    app.Run();
 
 }   
 
