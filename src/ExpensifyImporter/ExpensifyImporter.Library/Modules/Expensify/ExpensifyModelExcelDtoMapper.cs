f9e4e852bbcb3a0b51d3b0f77a73d762faf363ae
Matthew Basson <matthew.basson@lexisnexis.co.uk>
2022-09-25 19:45:31 +0100

    Getting Expenses to Excel Dto Map working and tested.

---
+++ src/ExpensifyImporter/ExpensifyImporter.Library/Modules/Expensify/ExpensifyModelExcelDtoMapper.cs
@@ -0,0 +1,91 @@
+﻿using DocumentFormat.OpenXml.Office2013.Excel;
+using ExpensifyImporter.Database.Domain;
+using ExpensifyImporter.Library.Modules.Excel;
+using ExpensifyImporter.Library.Modules.Excel.Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpensifyImporter.Library.Modules.Expensify
+{
+    public class ExpensifyModelExcelDtoMapper
+    {
+
+        private readonly ILogger<ExpensifyModelExcelDtoMapper> _logger;
+
+
+
+        public ExpensifyModelExcelDtoMapper(ILogger<ExpensifyModelExcelDtoMapper> logger)
+        {
+            _logger = logger;
+        }
+
+        public List<Expense> MapExpenses(List<ExcelSheet> excelBook, bool firstRowHasHeaders = true)
+        {
+            var expenses = new List<Expense>();
+
+            foreach (var excelSheet in excelBook)
+            {
+                int rowCounter = 0;
+                foreach (var excelRow in excelSheet)
+                {
+                    if(rowCounter == 0 && firstRowHasHeaders) {
+                        rowCounter++;
+                        continue;
+                    }
+                    var expense = new Expense();                    
+                    int cellCounter = 0;
+                    foreach(var excelCell in excelRow)
+                    {
+                        if (excelCell == null)
+                        {
+                            cellCounter++;
+                            continue;
+                        }                       
+
+                        switch (cellCounter)
+                        {
+                            case 0:
+                                expense.ExpenseId = int.Parse(excelCell.CellValue);
+                                break;
+                            case 1:
+                                expense.ReceiptId = int.Parse(excelCell.CellValue);
+                                break;
+                            case 2:
+                                expense.TransactionDateTime = DateTime.ParseExact(excelCell.CellValue, "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+                                break;
+                            case 3:
+                                expense.Merchant = excelCell.CellValue;
+                                break;
+                            case 4:
+                                expense.Amount = decimal.Parse(excelCell.CellValue);
+                                break;
+                            case 5:
+                                expense.Category = excelCell.CellValue;
+                                break;
+                            case 6:
+                                expense.Description = excelCell.CellValue;
+                                break;
+                            case 7:
+                                expense.ReceiptUrl = excelCell.CellValue;
+                                break;
+                        }
+
+                        cellCounter++;
+                    }
+                    expenses.Add(expense);
+                    rowCounter++;
+                }
+            }
+            return expenses;
+        }
+
+        
+
+    }
+}
---
+++ src/ExpensifyImporter/Tests/ExpensifyImporter.UnitTests/Modules/Expensify/ExpensifyModelExcelDtoMapperTests.cs
@@ -0,0 +1,137 @@
+﻿using ExpensifyImporter.Library.Modules.Excel.Domain;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using ExpensifyImporter.Library.Modules.Expensify;
+using ExpensifyImporter.Database.Domain;
+using System.Globalization;
+using FluentAssertions;
+
+namespace ExpensifyImporter.UnitTests.Modules.Expensify
+{
+    public class ExpensifyModelExcelDtoMapperTests
+    {
+        [Fact]
+        public void Mapping_ExcelBook_Collection_Yields_Correct_Expense_List()
+        {
+            //Arrange
+            var exccelPocoList = new List<ExcelSheet>()
+            {
+                new ExcelSheet()
+                {
+                    new ExcelRow()
+                    {
+                        new ExcelCell("ExpenseId"),
+                        new ExcelCell("ReceiptId"),
+                        new ExcelCell("TimeStamp"),
+                        new ExcelCell("Merchant"),
+                        new ExcelCell("Amount"),
+                        new ExcelCell("Category"),
+                        new ExcelCell("Description"),
+                        new ExcelCell("ReceiptUrl"),
+                    },
+                    new ExcelRow()
+                    {
+                        new ExcelCell("1"),
+                        new ExcelCell("1"),
+                        new ExcelCell("2017-04-01 12:00:00"),
+                        new ExcelCell("Costa"),
+                        new ExcelCell("11.1"),
+                        new ExcelCell("Meals"),
+                        new ExcelCell(""),
+                        new ExcelCell("https://www.expensify.com/receipts/w_8c12334126141c5a124ef131.jpg"),
+                    },
+                    new ExcelRow()
+                    {
+                        new ExcelCell("2"),
+                        new ExcelCell("2"),
+                        new ExcelCell("2017-04-02 12:00:00"),
+                        new ExcelCell("Mcdonalds"),
+                        new ExcelCell("12.2"),
+                        new ExcelCell("Meals"),
+                        new ExcelCell(""),
+                        new ExcelCell("https://www.expensify.com/receipts/w_8c12334126141c5a124ef132.jpg"),
+                    },
+                    new ExcelRow()
+                    {
+                        new ExcelCell("3"),
+                        new ExcelCell("3"),
+                        new ExcelCell("2017-04-03 12:00:00"),
+                        new ExcelCell("Eat"),
+                        new ExcelCell("13.3"),
+                        new ExcelCell("Meals"),
+                        new ExcelCell(""),
+                        new ExcelCell("https://www.expensify.com/receipts/w_8c12334126141c5a124ef133.jpg"),
+                    },
+                    new ExcelRow()
+                    {
+                        new ExcelCell("4"),
+                        new ExcelCell("4"),
+                        new ExcelCell("2017-04-04 12:00:00"),
+                        new ExcelCell("Wasabi"),
+                        new ExcelCell("14.4"),
+                        new ExcelCell("Meals"),
+                        new ExcelCell(""),
+                        new ExcelCell("https://www.expensify.com/receipts/w_8c12334126141c5a124ef134.jpg"),
+                    }
+
+                }
+            };
+
+            var mapper = new ExpensifyModelExcelDtoMapper(Substitute.For<ILogger<ExpensifyModelExcelDtoMapper>>());
+
+            var expectedExpenseList = new List<Expense>()
+            {
+                new Expense()
+                {                    
+                    ExpenseId=1,
+                    ReceiptId=1,
+                    TransactionDateTime = DateTime.ParseExact("2017-04-01 12:00:00","yyyy-MM-dd HH:mm:ss",CultureInfo.InvariantCulture),
+                    Merchant="Costa",
+                    Amount = 11.1M,
+                    Category ="Meals",
+                    Description="",
+                    ReceiptUrl = "https://www.expensify.com/receipts/w_8c12334126141c5a124ef131.jpg"
+                },
+                new Expense()
+                {                    
+                    ExpenseId=2,
+                    ReceiptId=2,
+                    TransactionDateTime = DateTime.ParseExact("2017-04-02 12:00:00","yyyy-MM-dd HH:mm:ss",CultureInfo.InvariantCulture),
+                    Merchant="Mcdonalds",
+                    Amount = 12.2M,
+                    Category ="Meals",
+                    Description="",
+                    ReceiptUrl = "https://www.expensify.com/receipts/w_8c12334126141c5a124ef132.jpg"
+                },
+                new Expense()
+                {                    
+                    ExpenseId=3,
+                    ReceiptId=3,
+                    TransactionDateTime = DateTime.ParseExact("2017-04-03 12:00:00","yyyy-MM-dd HH:mm:ss",CultureInfo.InvariantCulture),
+                    Merchant="Eat",
+                    Amount = 13.3M,
+                    Category ="Meals",
+                    Description="",
+                    ReceiptUrl = "https://www.expensify.com/receipts/w_8c12334126141c5a124ef133.jpg"
+                },
+                new Expense()
+                {                    
+                    ExpenseId=4,
+                    ReceiptId=4,
+                    TransactionDateTime = DateTime.ParseExact("2017-04-04 12:00:00","yyyy-MM-dd HH:mm:ss",CultureInfo.InvariantCulture),
+                    Merchant="Wasabi",
+                    Amount = 14.4M,
+                    Category ="Meals",
+                    Description="",
+                    ReceiptUrl = "https://www.expensify.com/receipts/w_8c12334126141c5a124ef134.jpg"
+                }
+            };
+
+            //Act 
+            var expenses = mapper.MapExpenses(exccelPocoList);
+
+            //Assert
+            expenses.Should().BeEquivalentTo(expectedExpenseList, opt => opt.Excluding(e => e.Id));
+        }
+    }
+}
