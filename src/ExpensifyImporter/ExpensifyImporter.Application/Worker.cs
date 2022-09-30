da982ef2b75613a6623b681b22e296fa0974a5da
Matthew Basson <matthew.basson@lexisnexis.co.uk>
2022-09-30 10:35:22 +0100

    Removing Flags and adding more logging.

--- src/ExpensifyImporter/ExpensifyImporter.Application/Worker.cs
+++ src/ExpensifyImporter/ExpensifyImporter.Application/Worker.cs
@@ -15,6 +15,7 @@ namespace ExpensifyImporter.Application
         private readonly FeatureFlagsConfiguration _featureConfiguration;
         private readonly ExcelToDatabaseSequencer _excelSequencer;
         private readonly DirectoryInfo _directoryInfo;
+        private readonly string _watchPath;
 
         public Worker(
             ILogger<Worker> logger,
@@ -27,8 +28,10 @@ namespace ExpensifyImporter.Application
             _workerConfiguration = workerConfiguration.Value;
             _featureConfiguration = featureConfiguration.Value;
             _excelSequencer = excelSequencer;
+            _watchPath = excelFileWatcher.Path;
             if (_featureConfiguration.WatchDirectory)
             {
+                _logger.LogInformation("Watch directory enabled listening file additions in path : {WatchPath}", _watchPath);
                 excelFileWatcher.Created += _excelFileWatcher_Created;
             }            
             _directoryInfo = new DirectoryInfo(excelFileWatcher.Path);
@@ -41,28 +44,29 @@ namespace ExpensifyImporter.Application
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            _logger.LogDebug("Debug : Watch directory enabled : {WatchDirectory}", _featureConfiguration.WatchDirectory);
+            _logger.LogDebug("Debug : Poll directory enabled : {PollDirectory}", _featureConfiguration.PollDirectory);
+            
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                _logger.LogInformation("Worker running at: {Time}", DateTimeOffset.Now);
 
                 if (_featureConfiguration.PollDirectory)
                 {
-                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    _logger.LogInformation("Poll directory enabled running ProcessExcelFiles for path: {WatchPath}", _watchPath);
                     await ProcessExcelFilesInPath();
                 }
-                
-
                 await Task.Delay(_workerConfiguration.Interval, stoppingToken);
             }
         }
 
         private async Task ProcessExcelFilesInPath()
         {
-            var files = _directoryInfo.GetFiles("*.xlsx");                      
+            var files = _directoryInfo.GetFiles("*.xlsx");
 
-            if(files != null && files.Any())
+            if(files.Any())
             {
-                _logger.LogInformation($"Procesing excel files, found {files.Count()}");
+                _logger.LogInformation("Processing excel files, found {Count}", files.Length);
                 foreach (var file in files)
                 {
                     await ProcessExcelFile(file.FullName);
@@ -72,11 +76,11 @@ namespace ExpensifyImporter.Application
 
         private async Task ProcessExcelFile(string path)
         {
-            _logger.LogInformation($"Procesing excel file: {path}");
+            _logger.LogInformation("Processing excel file: {Path}", path);
             var result = await _excelSequencer.ProcessDocumentAsync(path);
             if (result > 0)
             {
-                _logger.LogInformation($"Procesing excel file complete and saved: {path}");
+                _logger.LogInformation("Processing excel file complete and saved: {Path}", path);
                 File.Delete(path);
             }
         }
