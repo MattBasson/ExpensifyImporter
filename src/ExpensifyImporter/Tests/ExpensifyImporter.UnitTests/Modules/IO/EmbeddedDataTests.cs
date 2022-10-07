fdd2f6bbf4bc4edbe0c55016872544acb70b4286
Matthew Basson <matthew.basson@lexisnexis.co.uk>
2022-10-07 22:50:29 +0100

    Update to add image download tests with mock httpclient (using mock sadly trying to find concrete way)

--- src/ExpensifyImporter/Tests/ExpensifyImporter.UnitTests/Modules/IO/EmbeddedDataTests.cs
+++ src/ExpensifyImporter/Tests/ExpensifyImporter.UnitTests/Modules/IO/EmbeddedDataTests.cs
@@ -31,20 +31,22 @@ namespace ExpensifyImporter.UnitTests.Modules.IO
         }
         
         [Theory]
-        [InlineData("ExpensifyImporter.UnitTests.Modules.IO.Data.TestContent.txt")]
-        [InlineData("ExpensifyImporter.UnitTests.Modules.IO.Data.cat_cats_eyes_cat_face_269574.webp")]
-        [InlineData("ExpensifyImporter.UnitTests.Modules.IO.Data.cat_domestic_cat_sweet_269854.webp")]
-        [InlineData("ExpensifyImporter.UnitTests.Modules.IO.Data.cat_feline_cats_eye_220526.webp")]
-        public void Getting_Embedded_Data_Yields_Correct_File_And_ContentByteArray_With_Declared_Assembly(string path)
+        [InlineData("ExpensifyImporter.UnitTests.Modules.IO.Data.TestContent.txt",18)]
+        [InlineData("ExpensifyImporter.UnitTests.Modules.IO.Data.cat_cats_eyes_cat_face_269574.webp",64804)]
+        [InlineData("ExpensifyImporter.UnitTests.Modules.IO.Data.cat_domestic_cat_sweet_269854.webp",105220)]
+        [InlineData("ExpensifyImporter.UnitTests.Modules.IO.Data.cat_feline_cats_eye_220526.webp",89076)]
+        public void Getting_Embedded_Data_Yields_Correct_File_And_ContentByteArray_With_Declared_Assembly(string path, int sizeInBytes)
         {
             //Arrange
             var assembly = Assembly.GetAssembly(typeof(EmbeddedDataTests));
             //Act
             var fileContent = EmbeddedData.GetByteArray(path, assembly);
-
+            
             //Assert
             fileContent.Should().NotBeNull();
             fileContent.Length.Should().BePositive();
+            fileContent.Length.Should().BeInRange(sizeInBytes - 100, sizeInBytes + 100);
+            fileContent.Length.Should().Be(sizeInBytes);
 
         }
         
--- src/ExpensifyImporter/Tests/ExpensifyImporter.UnitTests/Modules/IO/ImageDownloaderTests.cs
+++ src/ExpensifyImporter/Tests/ExpensifyImporter.UnitTests/Modules/IO/ImageDownloaderTests.cs
@@ -1,20 +1,72 @@
-﻿namespace ExpensifyImporter.UnitTests.Modules.IO;
+﻿using System.Net;
+using System.Net.Http.Headers;
+using System.Reflection;
+using ExpensifyImporter.Library.Modules.IO;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Moq.Protected;
+using NSubstitute;
 
-public class ImageDownloaderTests
+namespace ExpensifyImporter.UnitTests.Modules.IO
 {
-    private readonly HttpClient _client;
-
-    public ImageDownloaderTests()
+    public class ImageDownloaderTests
     {
-        _client = new HttpClient();
-    }
+        private readonly HttpClient _client;
 
-    [Fact]
-    public async Task When_Image_Download_It_Successfully_Downloads_Image()
-    {
-        //https://images.all-free-download.com/images/graphicwebp/cat_domestic_cat_sweet_269854.webp
-        //https://images.all-free-download.com/images/graphicwebp/cat_feline_cats_eye_220526.webp
-        //https://images.all-free-download.com/images/graphicwebp/cat_cats_eyes_cat_face_269574.webp
-        
+        public ImageDownloaderTests()
+        {
+            _client = new HttpClient();
+        }
+
+        [Theory]
+        [InlineData("https://images.all-free-download.com/images/graphicwebp/cat_domestic_cat_sweet_269854.webp",
+            "cat_domestic_cat_sweet_269854.webp")]
+        [InlineData("https://images.all-free-download.com/images/graphicwebp/cat_feline_cats_eye_220526.webp",
+            "cat_feline_cats_eye_220526.webp")]
+        [InlineData("https://images.all-free-download.com/images/graphicwebp/cat_cats_eyes_cat_face_269574.webp",
+            "cat_cats_eyes_cat_face_269574.webp")]
+ 
+        public async Task When_Image_Download_It_Successfully_Downloads_Fake_Image(string url, string filename)
+        {
+            //Arrange 
+            var expectedFileByteArray =
+                EmbeddedData.GetByteArray($"ExpensifyImporter.UnitTests.Modules.IO.Data.{filename}",
+                    Assembly.GetAssembly(typeof(ImageDownloaderTests)));
+            
+            var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            handlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                ).ReturnsAsync(new HttpResponseMessage()
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new ByteArrayContent(expectedFileByteArray)
+                }).Verifiable();
+
+            var httpClient = new HttpClient(handlerMock.Object);
+
+            var sut = new ImageDownloader(Substitute.For<ILogger<ImageDownloader>>(), httpClient);
+
+            //Act
+            var result = await sut.ExecuteAsync(url);
+
+            //Assert
+
+            result.Should().Equal(expectedFileByteArray);
+                            
+            handlerMock.Protected().Verify(
+                "SendAsync",
+                Times.Exactly(1), // we expected a single external request
+                ItExpr.Is<HttpRequestMessage>(req =>
+                        req.Method == HttpMethod.Get  // we expected a GET request
+                        && req.RequestUri == new Uri(url) // to this uri
+                ),
+                ItExpr.IsAny<CancellationToken>()
+            );
+        }
     }
 }
