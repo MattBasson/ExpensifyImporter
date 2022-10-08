2b0d3af5ff607acce924afcd7c9326399833af5f
Matthew Basson <matthew.basson@lexisnexis.co.uk>
2022-10-08 13:24:31 +0100

    Moving FakeImageDownloaderHttpMessageHandler.cs to Fakes directory

---
+++ src/ExpensifyImporter/Tests/ExpensifyImporter.UnitTests/Modules/IO/Fakes/FakeImageDownloaderHttpMessageHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Reflection;
+using ExpensifyImporter.Library.Modules.IO;
+
+namespace ExpensifyImporter.UnitTests.Modules.IO.Fakes
+{
+    public class FakeImageDownloaderHttpMessageHandler : HttpMessageHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            //get the filename from the request
+            var fileName =
+                request.RequestUri?.OriginalString[(request.RequestUri.OriginalString.LastIndexOf("/", StringComparison.Ordinal)+1)..];
+            //Get the file byte array from embedded data.
+            var fileByteArray = await EmbeddedData.GetByteArrayAsync($"ExpensifyImporter.UnitTests.Modules.IO.Data.{fileName}",
+                Assembly.GetAssembly(typeof(ImageDownloaderTests)));
+            if (fileByteArray != null)
+            {
+                //return the fileByteArray if it matches the request
+                return new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new ByteArrayContent(fileByteArray)
+                };
+            }
+            //otherwise return no content
+            return new HttpResponseMessage(HttpStatusCode.NoContent);
+        }
+    }
+}
--- src/ExpensifyImporter/Tests/ExpensifyImporter.UnitTests/Modules/IO/ImageDownloaderTests.cs
+++ src/ExpensifyImporter/Tests/ExpensifyImporter.UnitTests/Modules/IO/ImageDownloaderTests.cs
@@ -1,6 +1,6 @@
-﻿using System.Net;
-using System.Reflection;
+﻿using System.Reflection;
 using ExpensifyImporter.Library.Modules.IO;
+using ExpensifyImporter.UnitTests.Modules.IO.Fakes;
 using FluentAssertions;
 using Microsoft.Extensions.Logging;
 
@@ -46,29 +46,6 @@ namespace ExpensifyImporter.UnitTests.Modules.IO
             result.Should().Equal(expectedFileByteArray);
 
             
-        }
-    }
-
-    public class FakeImageDownloaderHttpMessageHandler : HttpMessageHandler
-    {
-        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-        {
-            //get the filename from the request
-            var fileName =
-                request.RequestUri?.OriginalString[(request.RequestUri.OriginalString.LastIndexOf("/", StringComparison.Ordinal)+1)..];
-            //Get the file byte array from embedded data.
-            var fileByteArray = await EmbeddedData.GetByteArrayAsync($"ExpensifyImporter.UnitTests.Modules.IO.Data.{fileName}",
-                Assembly.GetAssembly(typeof(ImageDownloaderTests)));
-            if (fileByteArray != null)
-            {
-                //return the fileByteArray if it matches the request
-                return new HttpResponseMessage(HttpStatusCode.OK)
-                {
-                    Content = new ByteArrayContent(fileByteArray)
-                };
-            }
-            //otherwise return no content
-            return new HttpResponseMessage(HttpStatusCode.NoContent);
         }
     }
 }
