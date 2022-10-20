ff3403ce8e6aa4ed95fb4653342a7950b37c8c98
Matthew Basson <matthew.basson@lexisnexis.co.uk>
2022-10-20 08:34:02 +0100

    Adding some more tests around Expene image downloading.

--- src/ExpensifyImporter/Tests/ExpensifyImporter.UnitTests/Modules/Expensify/ExpensifyImageDownloaderTests.cs
+++ src/ExpensifyImporter/Tests/ExpensifyImporter.UnitTests/Modules/Expensify/ExpensifyImageDownloaderTests.cs
@@ -65,15 +65,14 @@ namespace ExpensifyImporter.UnitTests.Modules.Expensify
                 new(Guid.NewGuid(), Constants.CatImageUrl1)
             };
             const string EmbeddedDataPath = "ExpensifyImporter.UnitTests.Modules.IO.Data.";
+            var catImage1ByteArray = await EmbeddedData.GetByteArrayAsync(
+                $"{EmbeddedDataPath}{Constants.CatImageFile1}",
+                Assembly.GetAssembly(typeof(ExpensifyImageDownloaderTests)));
             var expectedResult = new List<ExpensifyImageDownloadResult>()
             {
-                new(batch[0].ExpenseId, await EmbeddedData.GetByteArrayAsync(
-                    $"{EmbeddedDataPath}{Constants.CatImageFile1}",
-                    Assembly.GetAssembly(typeof(ExpensifyImageDownloaderTests)))),
-                new (batch[1].ExpenseId, await EmbeddedData.GetByteArrayAsync($"{EmbeddedDataPath}{Constants.CatImageFile1}",
-                    Assembly.GetAssembly(typeof(ExpensifyImageDownloaderTests)))),
-                new (batch[2].ExpenseId, await EmbeddedData.GetByteArrayAsync($"{EmbeddedDataPath}{Constants.CatImageFile1}",
-                    Assembly.GetAssembly(typeof(ExpensifyImageDownloaderTests)))),
+                new(batch[0].ExpenseId, catImage1ByteArray),
+                new(batch[1].ExpenseId, catImage1ByteArray),
+                new(batch[2].ExpenseId, catImage1ByteArray),
             };
 
             var fakeImageDownloaderMessageHandler = new FakeImageDownloaderHttpMessageHandler();
@@ -88,6 +87,41 @@ namespace ExpensifyImporter.UnitTests.Modules.Expensify
             //Assert
 
             result.Any(a => a.FileContents == null).Should().BeFalse();
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+
+        [Fact]
+        public async Task Given_A_Batch_With_Null_Urls_It_Returns_All_Items_In_Batch()
+        {
+            //Arrange
+            var batch = new List<ExpenseImageBatchQueryResult>()
+            {
+                new(Guid.NewGuid(), null),
+                new(Guid.NewGuid(), null),
+                new(Guid.NewGuid(), null)
+            };
+            
+            var expectedResult = new List<ExpensifyImageDownloadResult>()
+            {
+                new(batch[0].ExpenseId, null),
+                    
+                new (batch[1].ExpenseId, null),
+                    
+                new (batch[2].ExpenseId,null),
+            };
+
+            var fakeImageDownloaderMessageHandler = new FakeImageDownloaderHttpMessageHandler();
+
+            var sut = new ExpensifyImageDownloader(
+                Substitute.For<ILogger<ExpensifyImageDownloader>>(),
+                new ImageDownloader(Substitute.For<ILogger<ImageDownloader>>(),
+                    new HttpClient(fakeImageDownloaderMessageHandler)));
+            //Act
+            var result = await sut.ExecuteAsync(batch);
+
+            //Assert
+
+            //result.Any(a => a.FileContents == null).Should().BeFalse();
             result.Should().BeEquivalentTo(expectedResult);
         }
     }
