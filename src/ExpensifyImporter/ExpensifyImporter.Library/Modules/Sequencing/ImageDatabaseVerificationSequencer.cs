8b453efaad546c333ada7d920ec35491c961b4a3
Matt Basson <matthew.basson@lexisnexis.co.uk>
2022-12-16 16:16:14 +0000

    Adding start of image verifier sequencer

---
+++ src/ExpensifyImporter/ExpensifyImporter.Library/Modules/Sequencing/ImageDatabaseVerificationSequencer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ExpensifyImporter.Library.Modules.Expensify;
+
+namespace ExpensifyImporter.Library.Modules.Sequencing
+{
+    public class ImageDatabaseVerificationSequencer
+    {
+        private readonly ILogger<ImageToDatabaseSequencer> _logger;
+        private readonly ExpenseImageBatchQuery _expenseImageBatchQuery;
+        private readonly ExpensifyImageDownloader _expensifyImageDownloader;
+        private readonly ExpenseImageBatchCommand _expenseImageBatchCommand;
+
+        public ImageDatabaseVerificationSequencer(
+            ILogger<ImageToDatabaseSequencer> logger,
+            ExpenseImageBatchQuery expenseImageBatchQuery,
+            ExpensifyImageDownloader expensifyImageDownloader,
+            ExpenseImageBatchCommand expenseImageBatchCommand)
+        {
+            _logger = logger;
+            _expenseImageBatchQuery = expenseImageBatchQuery;
+            _expensifyImageDownloader = expensifyImageDownloader;
+            _expenseImageBatchCommand = expenseImageBatchCommand;
+        }
+
+        public async Task<int> ProcessAsync(int batchSize = 0)
+        {
+            //Todo: add sequencing logic here.
+            return await Task.FromResult(0);
+        }
+    }
+}
+
