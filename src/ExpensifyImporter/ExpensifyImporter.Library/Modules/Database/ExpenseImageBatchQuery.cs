c2a79310ae38ea70f2c642a337a0dde0bd93e1b4
Matt Basson <matthew.basson@lexisnexis.co.uk>
2022-12-16 11:41:23 +0000

    Updating batch query to only include results with receipt urls

--- src/ExpensifyImporter/ExpensifyImporter.Library/Modules/Database/ExpenseImageBatchQuery.cs
+++ src/ExpensifyImporter/ExpensifyImporter.Library/Modules/Database/ExpenseImageBatchQuery.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.Linq.Expressions;
+
 using ExpensifyImporter.Database;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,7 +19,7 @@ namespace ExpensifyImporter.Library.Modules.Database
 
         public async Task<IEnumerable<ExpenseImageBatchQueryResult>> ExecuteAsync(int batchSize = 0)
         {
-            var queryable =  _dbContext.Expense.Where(expense => expense.ReceiptImage == null);
+            var queryable =  _dbContext.Expense.Where(expense => expense.ReceiptImage == null && expense.ReceiptUrl != null);
 
             if (batchSize > 0)
             {
