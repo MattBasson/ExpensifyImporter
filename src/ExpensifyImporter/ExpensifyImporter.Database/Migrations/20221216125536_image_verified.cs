67084367ff83b38c63395f0cc07fe6183bbea2bd
Matt Basson <matthew.basson@lexisnexis.co.uk>
2022-12-16 13:00:10 +0000

    Adding extra column for image verification

---
+++ src/ExpensifyImporter/ExpensifyImporter.Database/Migrations/20221216125536_image_verified.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpensifyImporter.Database.Migrations
+{
+    public partial class image_verified : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "image_verified",
+                table: "Expense",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "image_verified_datetime",
+                table: "Expense",
+                type: "DATETIME",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "image_verified",
+                table: "Expense");
+
+            migrationBuilder.DropColumn(
+                name: "image_verified_datetime",
+                table: "Expense");
+        }
+    }
+}
